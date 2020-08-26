465f48a315d295b9e857691157a4fafad0993a78
farzan <farzan.hosseiny@gmail.com>
2020-08-27 01:18:05 +0430

    changing folder and django app name

--- sampleApp/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-
-def showHomePage(request):
-    if request.method == "POST":
-        pass
-    return render(request, 'sampleApp/index.html')
-# Create your views here.
