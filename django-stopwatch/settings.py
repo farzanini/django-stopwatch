18a139434d5c6be14dd849a1edb5b5c2bbd4ce7d
farzan <farzan.hosseiny@gmail.com>
2020-08-27 01:40:03 +0430

    adding README.md

--- django-stopwatch/settings.py
+++ django-stopwatch/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '@i!b1p@(8p0=(t68647!g+(1&#n+nfnw88a$+ad!yqx7w7rptz'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
