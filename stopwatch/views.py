4eb27aef8b1186478dc5b59e8658a627dfdb74e4
Farzan Hosseini <farzan.hosseiny@gmail.com>
2020-08-29 16:50:29 +0430

    name fixing

--- stopwatch/views.py
+++ stopwatch/views.py
@@ -4,5 +4,5 @@ from django.shortcuts import render
 def showHomePage(request):
     if request.method == "POST":
         pass
-    return render(request, 'sampleApp/index.html')
+    return render(request, 'stopwatch/index.html')
 # Create your views here.
